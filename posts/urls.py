d743667e114486e29853b632b38fd5eeb026309a
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-10-24 15:00:24 -0400

    first commit

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import (
+    HomePageView, 
+    PostListView, 
+    PostDetailView
+)
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('posts/', PostListView.as_view(), name='all_posts'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+]
---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Post
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class PostListView(ListView):
+    model = Post
+    template_name= "posts_list.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
